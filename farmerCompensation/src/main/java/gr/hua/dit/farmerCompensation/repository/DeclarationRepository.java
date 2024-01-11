af523c5717952269a222df4b4adc96b8d4bda5b3
AlexandrosManolis <120735373+AlexandrosManolis@users.noreply.github.com>
2024-01-12 01:38:38 +0200

    Delete farmerCompensation directory

--- farmerCompensation/src/main/java/gr/hua/dit/farmerCompensation/repository/DeclarationRepository.java
+++
@@ -1,9 +0,0 @@
-package gr.hua.dit.farmerCompensation.repository;
-
-import gr.hua.dit.farmerCompensation.entity.DeclarationForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeclarationRepository extends JpaRepository<DeclarationForm, Integer> {
-}
