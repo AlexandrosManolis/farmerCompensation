27017bf27e4cd897cf2a08683a4d1556eccba979
ZenXaris03 <it2021101@hua.gr>
2024-08-12 21:30:17 +0300

    Testing files and code added

--- src/test/java/gr/hua/dit/farmerCompensation/FarmerCompensationApplicationTests.java
+++ src/test/java/gr/hua/dit/farmerCompensation/FarmerCompensationApplicationTests.java
@@ -1,13 +1,56 @@
 package gr.hua.dit.farmerCompensation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class FarmerCompensationApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void testCreateUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"email\":\"api@hua.gr\",\"password\":\"pass123\"}";
+
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signup")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk());
+	}
+
+	@Test
+	public void testSignUser() throws Exception {
+		// Arrange
+		String userJson = "{\"username\":\"apiuser\",\"password\":\"pass123\"}";
+
+		// Act
+		ResultActions result = mockMvc.perform(post("/api/auth/signin")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(userJson));
+
+		// Assert
+		result.andExpect(status().isOk())
+				.andExpect(jsonPath("$.username").value("apiuser"));
+
+	}
 }
