743dcdd514bddab520d82130314b6f726e264879
ZenXaris03 <it2021101@hua.gr>
2024-08-22 21:41:20 +0300

    security config reset changes

--- src/main/java/gr.hua.dit.farmerCompensation/config/SecurityConfig.java
+++ src/main/java/gr.hua.dit.farmerCompensation/config/SecurityConfig.java
@@ -20,47 +20,54 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true)
-public class SecurityConfig {
+public class SecurityConfig{
 
     @Autowired
     private AuthEntryPointJwt unauthorizedHandler;
-
     @Bean
     public AuthTokenFilter authenticationJwtTokenFilter() {
         return new AuthTokenFilter();
     }
 
     @Bean
-    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authConfig) throws Exception {
+    public AuthenticationManager authenticationManagerBean (AuthenticationConfiguration authConfig) throws Exception {
         return authConfig.getAuthenticationManager();
-    }
 
+    }
+    //set allowed methods, headers, and requestmatchers depending on the role of the user
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:9000"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        corsConfiguration.setAllowedHeaders(
+                List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:9000"));
+        corsConfiguration
+                .setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.setExposedHeaders(List.of("Authorization"));
 
         http
-            .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(request -> corsConfiguration))
-            .addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class)
-            .exceptionHandling(ex -> ex.authenticationEntryPoint(unauthorizedHandler))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/v3/api-docs/**", "/v2/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                .requestMatchers("/api/auth/**", "/actuator/health/**").permitAll()
-                .requestMatchers("/api/admin/users/**").hasRole("ADMIN")
-                .requestMatchers("/api/declaration/{user_id}/**").hasAnyRole("ADMIN", "INSPECTOR", "FARMER")
-                .requestMatchers("/api/users/**").hasAnyRole("ADMIN", "INSPECTOR", "FARMER")
-                .requestMatchers("/api/declaration/report/{user_id}/**").hasAnyRole("ADMIN", "INSPECTOR")
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .logout(logout -> logout.permitAll());
+                                        .csrf(csrf -> csrf.disable())
+                                        .cors(cors -> cors.configurationSource(request -> corsConfiguration))
+                                        .addFilterBefore(authenticationJwtTokenFilter(),UsernamePasswordAuthenticationFilter.class)
+                                        .exceptionHandling(ex -> ex
+                                                .authenticationEntryPoint(unauthorizedHandler))
+                                        .authorizeHttpRequests(auth -> auth
+                                                .requestMatchers("/v3/api-docs/**", "/v2/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                                                .requestMatchers("/api/auth/**","/actuator/health/**").permitAll()
+                                                .requestMatchers("/api/admin/users/**").hasRole("ADMIN")
+                                                .requestMatchers("/api/declaration/{user_id}/**").hasAnyRole("ADMIN", "INSPECTOR", "FARMER")
+                                                .requestMatchers("/api/users/**").hasAnyRole("ADMIN", "INSPECTOR", "FARMER")
+                                                .requestMatchers("/api/declaration/report/{user_id}/**").hasAnyRole("ADMIN", "INSPECTOR")
 
+                                        .anyRequest().authenticated()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout((logout) -> logout.permitAll());
+
+        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
 }
