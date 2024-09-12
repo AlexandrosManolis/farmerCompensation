59cd188944dbf0e1a318fcb38b9786a6070de2b1
ZenXaris03 <it2021101@hua.gr>
2024-09-12 22:45:23 +0300

    docker with mailhog:publicip

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,8 @@ IP=$(curl ifconfig.me)
 # Export the environment variable
 export VITE_BACKEND_URL="http://$IP:9090"
 
+# Export IP vlue
+export SPRING_IP=$IP
+
 # Execute the main command
 exec "$@"
