9a3cfe72a05c509c5c92cbbcf356d881495eb87f
AlexandrosManolis <it2021132@hua.gr>
2024-09-15 18:04:29 +0300

    add 8025 port

--- src/main/java/gr.hua.dit.farmerCompensation/config/SecurityConfig.java
+++ src/main/java/gr.hua.dit.farmerCompensation/config/SecurityConfig.java
@@ -42,6 +42,7 @@ public class SecurityConfig{
                 List.of("Authorization", "Cache-Control", "Content-Type"));
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
         corsConfiguration.setAllowedOrigins(List.of("http://localhost:9000"));
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:8025"));
         corsConfiguration
                 .setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
         corsConfiguration.setAllowCredentials(true);
